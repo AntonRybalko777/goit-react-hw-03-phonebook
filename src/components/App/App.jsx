a69363ade8a0ce4740d64e423bc27c539ece9ae4
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-10-10 23:38:18 +0300

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,6 +11,19 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   checkDuplicate = enteredName => {
     return this.state.contacts.find(contact => {
       return contact.name === enteredName;
